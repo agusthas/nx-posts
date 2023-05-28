bae9e7ebc551691effb5924c956318d78d0409c6
SusuMantan <suzumantan@gmail.com>
2023-05-29 02:46:01 +0700

    client: add tailwindcss

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     <div>
-      <h1>
+      <h1 className="text-3xl text-red-500 font-bold text-center">
         <span> Hello there, </span>
         Welcome client 👋
       </h1>
